cc6acd0bcc73bc737423a00b9f9b8804fe4039f9
ds <2601948@gmail.com>
2023-03-12 05:22:56 +0900

    JanDer
    - 1편: 잔디의 힘
    - 2편: 잔디 원정대
    - 3편: 잔디왕의 귀환

--- api-server/matching/src/main/java/com/soon83/infrastructure/member/MemberMatchingConditionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soon83.infrastructure.member;
-
-import com.soon83.domain.member.matchingcondition.MemberMatchingCondition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberMatchingConditionRepository extends JpaRepository<MemberMatchingCondition, Long> {
-}
