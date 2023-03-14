864e051abbef6086b35fa543584bcded0f1e0323
ds <2601948@gmail.com>
2023-03-14 13:45:03 +0900

    어이 관상가 양반, 내가 야근왕이 될 상인가? -> 야근왕은 되시지만 야근수당은 못 받는 관상입니다,,

--- api-server/matching/src/main/java/com/soon83/domain/receivemessage/ReceiveMessageQuery.java
+++ api-server/matching/src/main/java/com/soon83/domain/receivemessage/ReceiveMessageQuery.java
@@ -8,24 +8,36 @@ public class ReceiveMessageQuery {
     @Data
     public static class Main {
         private final Long id;
+        private boolean hiddenFromSender;
+        private boolean hiddenFromTargetMember;
         private final Long targetMemberId;
         private final Long messageId;
+        private final Long notificationId;
 
         @Builder
         public Main(
                 Long id,
+                boolean hiddenFromSender,
+                boolean hiddenFromTargetMember,
                 Long targetMemberId,
-                Long messageId
+                Long messageId,
+                Long notificationId
         ) {
             this.id = id;
+            this.hiddenFromSender = hiddenFromSender;
+            this.hiddenFromTargetMember = hiddenFromTargetMember;
             this.targetMemberId = targetMemberId;
             this.messageId = messageId;
+            this.notificationId = notificationId;
         }
 
         public Main(ReceiveMessage entity) {
             this.id = entity.getId();
+            this.hiddenFromSender = entity.isHiddenFromSender();
+            this.hiddenFromTargetMember = entity.isHiddenFromTargetMember();
             this.targetMemberId = entity.getTargetMember().getId();
             this.messageId = entity.getMessage().getId();
+            this.notificationId = entity.getNotification().getId();
         }
     }
 }
--- api-server/matching/src/main/java/com/soon83/interfaces/receivemessage/ReceiveMessageDto.java
+++ api-server/matching/src/main/java/com/soon83/interfaces/receivemessage/ReceiveMessageDto.java
@@ -9,24 +9,36 @@ public class ReceiveMessageDto {
     @Data
     public static class Main {
         private final Long receiveMessageId;
+        private boolean hiddenFromSender;
+        private boolean hiddenFromTargetMember;
         private final Long targetMemberId;
         private final Long messageId;
+        private final Long notificationId;
 
         @Builder
         public Main(
                 Long receiveMessageId,
+                boolean hiddenFromSender,
+                boolean hiddenFromTargetMember,
                 Long targetMemberId,
-                Long messageId
+                Long messageId,
+                Long notificationId
         ) {
             this.receiveMessageId = receiveMessageId;
+            this.hiddenFromSender = hiddenFromSender;
+            this.hiddenFromTargetMember = hiddenFromTargetMember;
             this.targetMemberId = targetMemberId;
             this.messageId = messageId;
+            this.notificationId = notificationId;
         }
 
         public Main(ReceiveMessageQuery.Main main) {
             this.receiveMessageId = main.getId();
+            this.hiddenFromSender = main.isHiddenFromSender();
+            this.hiddenFromTargetMember = main.isHiddenFromTargetMember();
             this.targetMemberId = main.getTargetMemberId();
             this.messageId = main.getMessageId();
+            this.notificationId = main.getNotificationId();
         }
     }
 }
